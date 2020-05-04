34354b74d1b17aef037b9b93464b160dcbe80c67
Franco Giordani <franco@space.bar>
2020-05-04 03:18:19 -0500

    webhook events

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,6 +4,11 @@ const express = require('express')
 const router = express.Router()
 const { webhook } = require('../src/webhook')
 
+router.post('/webhookbuy', webhook.webhookbuy)
+router.post('/webhookbuycancel', webhook.webhookbuycancel)
+router.post('/webhooksell', webhook.webhooksell)
+router.post('/webhooksellcancel', webhook.webhooksellcancel)
+router.post('/webhookstatus', webhook.webhookstatus)
 router.all('/', webhook)
 
 module.exports = router
--- src/webhook/webhook.js
+++ src/webhook/webhook.js
@@ -5,13 +5,31 @@ const action = {}
 const moment = require('moment')
 moment().format()
 
-/**
- * Validate Token
- */
 action.webhook = (req, res) => {
   log.debug('webhook route requested')
   log.debug(req.params)
   log.debug(JSON.stringify(req.params))
+  res.status(200).end()
+}
+
+action.webhookbuy = (req, res) => {
+  console.log(JSON.stringify(req.body))
+  res.status(200).end()
+}
+action.webhookbuycancel = (req, res) => {
+  console.log(JSON.stringify(req.body))
+  res.status(200).end()
+}
+action.webhooksell = (req, res) => {
+  console.log(JSON.stringify(req.body))
+  res.status(200).end()
+}
+action.webhooksellcancel = (req, res) => {
+  console.log(JSON.stringify(req.body))
+  res.status(200).end()
+}
+action.webhookstatus = (req, res) => {
+  console.log(JSON.stringify(req.body))
   res.status(200).end()
 }
 
